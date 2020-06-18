828f88358a4a0038d9f027249bfc44ab2e9889a0
허성수 <gjtjdtn201@naver.com>
2020-06-18 12:53:26 +0900

    deploy setting

--- moviepjt/moviepjt/settings/base.py
+++ moviepjt/moviepjt/settings/base.py
@@ -14,19 +14,14 @@ import os
 from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -133,7 +128,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'blogapp', 'static')
 ]
  
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 AUTH_USER_MODEL = 'accounts.User'
 
---
+++ moviepjt/moviepjt/settings/production.py
@@ -0,0 +1,8 @@
+from .base import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = [
+    '.compute.amazonaws.com',
+    '54.180.160.164',
+]
