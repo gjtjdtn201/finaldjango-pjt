00a962947eb5bd7311ab0df8f78056cc45345665
허성수 <gjtjdtn201@naver.com>
2020-06-18 23:19:27 +0900

    0618 font fix

--- moviepjt/accounts/urls.py
+++ moviepjt/accounts/urls.py
@@ -7,7 +7,7 @@ urlpatterns=[
     path('signup/', views.signup, name='signup'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
-    path('<str:username>/', views.detail, name='detail'),
+    path('detail/<str:username>/', views.detail, name='detail'),
     path('<str:username>/follow', views.follow, name='follow'),
     path('<int:pk>/genlike/', views.genlike, name="genlike"),
     ]
